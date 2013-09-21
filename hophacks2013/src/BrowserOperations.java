c5181526a2c11dd43052f9d00722ec4d3ba7c3ba
Julia <juliadeutsch@jhu.edu>
2013-09-21 04:33:56 -0400

    category class files

---
+++ hophacks2013/src/BrowserOperations.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Julia
+ */
+import java.io.IOException;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserOperations {
+    
+    public static void main(String[] args) throws Exception {
+        
+        Process process = null;
+        Runtime runtime = null;
+        
+        try {
+        runtime = Runtime.getRuntime();
+        process = runtime.exec("open /Applications/Firefox.app");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.facebook.com");
+            
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("amonroe7@jhu.edu");
+        
+        WebElement password = driver.findElement(By.name("pass"));
+        password.sendKeys("hophacks");
+        
+        WebElement submit = driver.findElement(By.id("u_0_b"));
+        submit.click();
+        
+    }   
+    
+}
